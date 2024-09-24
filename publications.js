4dc8e9bd02f852184b561efd1fa9473cd72ee0a9
xhdu <2571167@qq.com>
2024-09-25 00:03:19 +0800

    Initial commit

---
+++ publications.js
@@ -0,0 +1,49 @@
+// publications.js
+
+const publications = [
+    {
+        title: "Generalization-Enhanced Code Vulnerability Detection via Multi-Task Instruction Fine-Tuning",
+        link: "https://aclanthology.org/2024.findings-acl.625",
+        authors: "<b>Xiaohu Du</b>, Ming Wen, Jiahao Zhu, Zifan Xie, Bin Ji, Huijun Liu, Xuanhua Shi, and Hai Jin.",
+        conference: "In Findings of ACL 2024.",
+        pdf: "http://xhdu.github.io/files/ACL24.pdf",
+        code: "https://github.com/CGCL-codes/VulLLM",
+        selected: true
+    },
+    {
+        title: "An Extensive Study on Adversarial Attack against Pre-trained Models of Code",
+        link: "https://dl.acm.org/doi/10.1145/3611643.3616356",
+        authors: "<b>Xiaohu Du</b>, Ming Wen, Zichao Wei, Shangwen Wang, and Hai Jin.",
+        conference: "In Proceedings of ESEC/FSE 2023, <span style='color: red;'>CCF-A</span>",
+        pdf: "http://xhdu.github.io/files/FSE23-1.pdf",
+        code: "https://github.com/CGCL-codes/Attack_PTMC",
+        selected: true
+    },
+    {
+        title: "Understanding the Bug Characteristics and Fix Strategies of Federated Learning Systems",
+        link: "https://dl.acm.org/doi/10.1145/3611643.3616347",
+        authors: "<b>Xiaohu Du</b>, Xiao Chen, Jialun Cao, Ming Wen, Shing-Chi Cheung, and Hai Jin.",
+        conference: "In Proceedings of ESEC/FSE 2023, <span style='color: red;'>CCF-A</span>",
+        pdf: "http://xhdu.github.io/files/FSE23-2.pdf",
+        code: "https://github.com/CGCL-codes/FL_Bug_Study",
+        selected: true
+    },
+    {
+        title: "Combating Word-level Adversarial Text with Robust Adversarial Training",
+        link: "https://ieeexplore.ieee.org/document/9533725",
+        authors: "<b>Xiaohu Du</b>, Jie Yu, Shasha Li, Zibo Yi, Hai Liu, and Jun Ma.",
+        conference: "In Proceedings of IJCNN 2021, CCF-C",
+        pdf: "http://xhdu.github.io/files/IJCNN21.pdf",
+        code: "#",
+        selected: false
+    },
+    {
+        title: "Adversarial Text Attack and Defense: A Review",
+        link: "http://jcip.cipsc.org.cn/CN/Y2021/V35/I8/1",
+        authors: "<b>Xiaohu Du</b>, Hongming Wu, Zibo Yi, Shasha Li, Jun Ma, and Jie Yu.",
+        conference: "In Journal of Chinese Information Processing, CCF-B in Chinese",
+        pdf: "http://xhdu.github.io/files/JCIP21.pdf",
+        code: "#",
+        selected: false
+    }
+];
